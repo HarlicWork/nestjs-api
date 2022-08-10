7e006188755ae66726a542d28a3b6fb07f4399a3
HarlicWork <harlic.work@gmail.com>
2022-08-10 16:04:40 +0800

    add usercontroller

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -8,7 +8,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-
       secretOrKey: config.get('JWT_SECRET'),
     });
   }
